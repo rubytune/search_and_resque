a8249d4530843f05edf4c0cc5d71397db0b02493
Peter Woo <petersnowdonwoo@gmail.com>
2014-12-04 17:05:36 -0800

    More tests

--- test/search_and_resque_test.rb
+++ test/search_and_resque_test.rb
@@ -20,7 +20,8 @@ end
 
 def setup!
   connect!
-  ActiveRecord::Base.connection.execute 'CREATE TABLE books (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME, title STRING, content STRING)'
+  ActiveRecord::Base.connection.execute 'CREATE TABLE books (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME, title STRING)'
+  ActiveRecord::Base.connection.execute 'CREATE TABLE films (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME, title STRING, director STRING)'
 end
 
 setup!
@@ -29,10 +30,18 @@ class Book < ActiveRecord::Base
   search_and_resque :books
 end
 
+class Film < ActiveRecord::Base
+  search_and_resque :films, :if => ->{ title_changed? }
+end
+
 class SearchAndResqueTestIndex < Chewy::Index
   define_type Book, :name => 'books' do
     field :title
-    field :content
+  end
+
+  define_type Film, :name => 'films' do
+    field :title
+    field :director
   end
 end
 
@@ -56,9 +65,30 @@ class SearchAndResqueTest < test_framework
 
   def test_index_update_on_create_and_destroy
     book = Book.create
-    assert_equal 1, SearchAndResqueTestIndex::Books.total_count
+    assert_equal 1, SearchAndResqueTestIndex::Books.filter(:ids => {:values => [book.id]}).total_count
 
     book.destroy
-    assert_equal 0, SearchAndResqueTestIndex::Books.total_count
+    assert_equal 0, SearchAndResqueTestIndex::Books.filter(:ids => {:values => [book.id]}).total_count
+  end
+
+  def test_index_update_on_save
+    book = Book.create(:title => 'one')
+
+    book.update_attributes(:title => 'two')
+    assert_equal 'two', SearchAndResqueTestIndex::Books.filter(:ids => {:values => [book.id]}).first.title
+  end
+
+  def test_failing_conditional_index_update
+    film = Film.create(:title => 'one', :director => 'two')
+
+    film.update_attributes(:director => 'three')
+    assert_equal 'two', SearchAndResqueTestIndex::Films.filter(:ids => {:values => [film.id]}).first.director
+  end
+
+  def test_succeeding_conditional_index_update
+    film = Film.create(:title => 'one', :director => 'two')
+
+    film.update_attributes(:title => 'three', :director => 'four')
+    assert_equal 'four', SearchAndResqueTestIndex::Films.filter(:ids => {:values => [film.id]}).first.director
   end
 end
